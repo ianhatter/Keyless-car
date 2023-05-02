1788183c08137241246c4867bd212cc86331b6d8
ianhatter <102544270+ianhatter@users.noreply.github.com>
2023-05-02 14:59:21 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//function checkdriversage(){
+	var age = prompt("what is your age");
+
+if (age <18){
+	alert("sorry your too young powering off");
+} else if (age >18){
+	 alert("powering on! welcome enjoy your ride");
+}   
+else if (age == 18){
+	alert("powering on! welcome enjoy your first year of driving");
+}
+//}
+//checkdriversage();
+
